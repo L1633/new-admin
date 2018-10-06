be945740c9740cf9523cf4f09a2a96a628351291
111 <719886065@qq.com>
2018-10-06 15:16:01 +0800

    课程编辑

--- src/pages/course/editCourse.vue
+++ src/pages/course/editCourse.vue
@@ -13,6 +13,9 @@
             </swiper-slide>
         </swiper>
 
+        <div class="detail-form">
+            
+        </div>
     </div>
 </template>
 
@@ -51,7 +54,8 @@
             height: 1.6rem;
             background-color: #fff;
             .slider-item{
-                
+                width: 100%;
+                height: 100%;
             }
         }
     }
